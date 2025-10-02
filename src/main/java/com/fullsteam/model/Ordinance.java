f63b344309b42e3159e4d74a6b989a500233bfc9
ryeash <ryan.yeash@broadcom.com>
2025-10-01 19:43:18 -0600

    wip

--- src/main/java/com/fullsteam/model/Ordinance.java
+++ src/main/java/com/fullsteam/model/Ordinance.java
@@ -11,9 +11,9 @@ public enum Ordinance {
     FLAMETHROWER(8, "Short-range fire stream", 3.5, 0.8, false, 0.4, 10.0, null, 0.0, 0.0),
     
     // Beam-based ordinance
-    LASER(25, "Instant-hit beam weapon", 1.0, 2.0, false, 0.8, 0.0, DamageApplicationType.INSTANT, 0.0, 1.5),
-    PLASMA_BEAM(20, "Continuous damage beam", 1.5, 1.5, true, 1.0, 0.0, DamageApplicationType.DAMAGE_OVER_TIME, 0.1, 2.0),
-    HEAL_BEAM(15, "Continuous healing beam", 1.0, 1.0, true, 1.2, 0.0, DamageApplicationType.DAMAGE_OVER_TIME, 0.05, 3.0),
+    LASER(25, "Instant-hit beam weapon", 1.0, 2.0, false, 0.8, 0.0, DamageApplicationType.INSTANT, 0.0, .3),
+    PLASMA_BEAM(20, "Continuous damage beam", 1.5, 1.5, false, 1.0, 0.0, DamageApplicationType.DAMAGE_OVER_TIME, 0.1, 2.0),
+    HEAL_BEAM(15, "Continuous healing beam", 1.0, 1.0, false, 1.2, 0.0, DamageApplicationType.DAMAGE_OVER_TIME, 0.05, 1.0),
     RAILGUN(30, "Piercing instant beam", 0.5, 3.0, false, 0.5, 0.0, DamageApplicationType.INSTANT, 0.0, 0.8),
     PULSE_LASER(22, "Burst damage beam", 1.2, 1.8, true, 0.9, 0.0, DamageApplicationType.BURST, 0.2, 1.5),
     ARC_BEAM(18, "Chain lightning beam", 1.3, 1.6, true, 1.1, 0.0, DamageApplicationType.BURST, 0.3, 2.5);
